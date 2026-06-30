e666987419721edd2801f21efd8df876032a5328
domblocs-dev <dominique@quatrium.fr>
2026-06-30 17:21:48 +0200

    Ajout des entités métier et de la première migration EF Core (étape 3)

---
+++ ExpressVoitures/Data/Migrations/20260630150259_AjoutEntitesMetier.cs
@@ -0,0 +1,197 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace ExpressVoitures.Data.Migrations
+{
+    /// <inheritdoc />
+    public partial class AjoutEntitesMetier : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Marques",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Nom = table.Column<string>(type: "nvarchar(450)", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Marques", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "TypeReparations",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Libelle = table.Column<string>(type: "nvarchar(450)", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_TypeReparations", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Modeles",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Nom = table.Column<string>(type: "nvarchar(450)", nullable: false),
+                    MarqueId = table.Column<int>(type: "int", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Modeles", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_Modeles_Marques_MarqueId",
+                        column: x => x.MarqueId,
+                        principalTable: "Marques",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Voitures",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    CodeVIN = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    Finition = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    Annee = table.Column<int>(type: "int", nullable: false),
+                    ModeleId = table.Column<int>(type: "int", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Voitures", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_Voitures_Modeles_ModeleId",
+                        column: x => x.ModeleId,
+                        principalTable: "Modeles",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Ventes",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    VoitureId = table.Column<int>(type: "int", nullable: false),
+                    PrixAchat = table.Column<decimal>(type: "decimal(18,2)", precision: 18, scale: 2, nullable: false),
+                    PrixVente = table.Column<decimal>(type: "decimal(18,2)", precision: 18, scale: 2, nullable: false),
+                    DateAchat = table.Column<DateOnly>(type: "date", nullable: false),
+                    DateDisponibilite = table.Column<DateOnly>(type: "date", nullable: true),
+                    DateVente = table.Column<DateOnly>(type: "date", nullable: true),
+                    Description = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    PhotoUrl = table.Column<string>(type: "nvarchar(max)", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Ventes", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_Ventes_Voitures_VoitureId",
+                        column: x => x.VoitureId,
+                        principalTable: "Voitures",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Reparations",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Cout = table.Column<decimal>(type: "decimal(18,2)", precision: 18, scale: 2, nullable: false),
+                    VenteId = table.Column<int>(type: "int", nullable: false),
+                    TypeReparationId = table.Column<int>(type: "int", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Reparations", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_Reparations_TypeReparations_TypeReparationId",
+                        column: x => x.TypeReparationId,
+                        principalTable: "TypeReparations",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                    table.ForeignKey(
+                        name: "FK_Reparations_Ventes_VenteId",
+                        column: x => x.VenteId,
+                        principalTable: "Ventes",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Marques_Nom",
+                table: "Marques",
+                column: "Nom",
+                unique: true);
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Modeles_MarqueId_Nom",
+                table: "Modeles",
+                columns: new[] { "MarqueId", "Nom" },
+                unique: true);
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Reparations_TypeReparationId",
+                table: "Reparations",
+                column: "TypeReparationId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Reparations_VenteId",
+                table: "Reparations",
+                column: "VenteId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_TypeReparations_Libelle",
+                table: "TypeReparations",
+                column: "Libelle",
+                unique: true);
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Ventes_VoitureId",
+                table: "Ventes",
+                column: "VoitureId",
+                unique: true);
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Voitures_ModeleId",
+                table: "Voitures",
+                column: "ModeleId");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Reparations");
+
+            migrationBuilder.DropTable(
+                name: "TypeReparations");
+
+            migrationBuilder.DropTable(
+                name: "Ventes");
+
+            migrationBuilder.DropTable(
+                name: "Voitures");
+
+            migrationBuilder.DropTable(
+                name: "Modeles");
+
+            migrationBuilder.DropTable(
+                name: "Marques");
+        }
+    }
+}
